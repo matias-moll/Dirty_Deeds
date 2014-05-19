6f5fd8e62858d22198d208e8b8e0a79490d2bc8f
Jon Village <contact@jonvillage.com>
2014-05-19 17:54:16 -0300

    migrated factura

--- Base de Datos/data/script_migracion_datos.sql
+++ Base de Datos/data/script_migracion_datos.sql
@@ -1,3 +1,7 @@
 INSERT INTO DIRTYDEEDS.FormaPago (Descripcion) 
 SELECT DISTINCT Forma_Pago_Desc FROM gd_esquema.Maestra 
 WHERE Forma_Pago_Desc IS NOT NULL;
+
+INSERT INTO DIRTYDEEDS.Factura (Numero, Fecha, Total, IdFormaPago)
+SELECT DISTINCT Factura_Nro, Factura_Fecha, Factura_Total,Id FROM gd_esquema.Maestra, DIRTYDEEDS.FormaPago 
+WHERE Forma_Pago_Desc = Descripcion AND Factura_Nro IS NOT NULL;
